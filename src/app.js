8f915fdf9aeb4b783bca1b05900117d0881a2395
jjkun <juan.jose1983@gmail.com>
2026-02-12 17:03:54 -0600

    Fix wel-know

--- src/app.js
+++ src/app.js
@@ -2,17 +2,19 @@ require('dotenv').config();
 const express = require('express');
 const authRoutes = require('./routes/auth.routes');
 const protectedRoutes = require('./routes/protected.routes');
+const authController = require('./controllers/auth.controller');
 
 const app = express();
 
 app.use(express.json());
 app.use('/auth', authRoutes);
-
 app.use('/protected', protectedRoutes);
 
 app.get('/health', (req, res) => {
   res.json({ status: 'ok' });
 });
+
+app.get('/.well-known/jwks.json', authController.jwks);
 
 
 
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,6 +3,5 @@ const router = express.Router();
 const authController = require('../controllers/auth.controller');
 
 router.post('/login', authController.login);
-router.get('/.well-known/jwks.json', authController.jwks);
 
 module.exports = router;
