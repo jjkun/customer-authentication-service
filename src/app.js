356e681aea9b036082ae812b8c6bb4eca81216bd
jjkun <juan.jose1983@gmail.com>
2026-02-12 17:54:36 -0600

    Adding configuration

--- src/app.js
+++ src/app.js
@@ -16,6 +16,15 @@ app.get('/health', (req, res) => {
 
 app.get('/.well-known/jwks.json', authController.jwks);
 
+app.get('/.well-known/openid-configuration', (req, res) => {
+  res.json({
+    issuer: 'https://kacy-promonopolistic-unturgidly.ngrok-free.dev',
+    jwks_uri: 'https://kacy-promonopolistic-unturgidly.ngrok-free.dev/.well-known/jwks.json',
+    response_types_supported: ['token'],
+    subject_types_supported: ['public'],
+    id_token_signing_alg_values_supported: ['RS256']
+  });
+});
 
 
 const PORT = process.env.PORT || 3000;
